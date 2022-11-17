83b1d56f4f392eb80c26d333fe4b4a4a606d316a
starniksm <starniknik17@gmail.com>
2022-11-17 13:07:07 +0300

    do it3

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-6rj!%z@f2i!7!vb31qw9osykircnsgr@6jwj4dj3z!-m_1tx9m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'starnik']
 
 
 # Application definition
